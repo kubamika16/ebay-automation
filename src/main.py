c19800eb3318124f4979a401b31f3390526e4edb
kubamika16 <kuba.mika16@gmail.com>
2024-09-10 17:44:05 +0100

    test - time increase

--- src/main.py
+++ src/main.py
@@ -44,7 +44,7 @@ def main(event, context):
     for iphone in iphones:
         print(f"Processing {iphone['name']}")
         ebay_items = process_ebay_items(
-            iphone['name'], iphone['min_price'], iphone['max_price'], 1000)
+            iphone['name'], iphone['min_price'], iphone['max_price'], 2000)
         all_items.extend(ebay_items)  # Combine results from each iPhone model
 
      # Filter out undervalued items based on their condition status
