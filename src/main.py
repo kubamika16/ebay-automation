14aedbea614b45b75f44928f7c325861a58b32dc
kubamika16 <kuba.mika16@gmail.com>
2024-09-10 18:02:27 +0100

    test

--- src/main.py
+++ src/main.py
@@ -12,27 +12,31 @@ def main(event, context):
     print('Hello!')
     # List of iPhones with model name and price range (in GBP), sorted from iPhone 8 to iPhone 12
     iphones = [
-        {'name': 'iPhone 8 64GB', 'min_price': 0, 'max_price': 435},
-        {'name': 'iPhone 8 128GB', 'min_price': 10, 'max_price': 460},
-        {'name': 'iPhone 8 Plus 64GB', 'min_price': 10, 'max_price': 460},
-        {'name': 'iPhone 8 Plus 128GB', 'min_price': 20, 'max_price': 470},
-        {'name': 'iPhone X 64GB', 'min_price': 20, 'max_price': 470},
-        {'name': 'iPhone X 256GB', 'min_price': 30, 'max_price': 490},
-        {'name': 'iPhone XR 64GB', 'min_price': 20, 'max_price': 470},
-        {'name': 'iPhone XR 128GB', 'min_price': 40, 'max_price': 500},
-        {'name': 'iPhone XS 64GB', 'min_price': 40, 'max_price': 500},
-        {'name': 'iPhone XS 256GB', 'min_price': 40, 'max_price': 500},
-        {'name': 'iPhone XS Max 64GB', 'min_price': 50, 'max_price': 510},
-        {'name': 'iPhone XS Max 256GB', 'min_price': 50, 'max_price': 510},
-        {'name': 'iPhone 11 64GB', 'min_price': 50, 'max_price': 510},
-        {'name': 'iPhone 11 128GB', 'min_price': 60, 'max_price': 520},
-        {'name': 'iPhone 12 64GB', 'min_price': 90, 'max_price': 550},
-        {'name': 'iPhone 12 128GB', 'min_price': 100, 'max_price': 560},
-        {'name': 'iPhone 12 Pro 128GB', 'min_price': 100, 'max_price': 580},
-        {'name': 'iPhone 12 Pro 256GB', 'min_price': 90, 'max_price': 600}
+        {'name': 'iPhone 8 64GB', 'min_price': 20, 'max_price': 35},
+        {'name': 'iPhone 8 128GB', 'min_price': 30, 'max_price': 60},
+        {'name': 'iPhone 8 Plus 64GB', 'min_price': 30, 'max_price': 60},
+        {'name': 'iPhone 8 Plus 128GB', 'min_price': 40, 'max_price': 70},
+
+        {'name': 'iPhone X 64GB', 'min_price': 40, 'max_price': 70},
+        {'name': 'iPhone X 256GB', 'min_price': 50, 'max_price': 90},
+
+        {'name': 'iPhone XR 64GB', 'min_price': 40, 'max_price': 70},
+        {'name': 'iPhone XR 128GB', 'min_price': 60, 'max_price': 100},
+
+        {'name': 'iPhone XS 64GB', 'min_price': 60, 'max_price': 100},
+        {'name': 'iPhone XS 256GB', 'min_price': 60, 'max_price': 100},
+        {'name': 'iPhone XS Max 64GB', 'min_price': 70, 'max_price': 110},
+        {'name': 'iPhone XS Max 256GB', 'min_price': 70, 'max_price': 110},
+
+        {'name': 'iPhone 11 64GB', 'min_price': 70, 'max_price': 110},
+        {'name': 'iPhone 11 128GB', 'min_price': 80, 'max_price': 120},
+
+        {'name': 'iPhone 12 64GB', 'min_price': 110, 'max_price': 150},
+        {'name': 'iPhone 12 128GB', 'min_price': 120, 'max_price': 160},
+
+        {'name': 'iPhone 12 Pro 128GB', 'min_price': 120, 'max_price': 180},
+        {'name': 'iPhone 12 Pro 256GB', 'min_price': 110, 'max_price': 200}
     ]
-
-
 
     all_items = []
 
